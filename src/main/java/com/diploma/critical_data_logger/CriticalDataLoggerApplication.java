86052fc3e6f50d22a5b0b97d5b5281bd1ee2aa24
Yevhenii Rud <zhenniarud@gmail.com>
2025-04-08 11:00:29 +0300

    first commit

---
+++ src/main/java/com/diploma/critical_data_logger/CriticalDataLoggerApplication.java
@@ -0,0 +1,13 @@
+package com.diploma.critical_data_logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriticalDataLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriticalDataLoggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diploma/critical_data_logger/CriticalDataLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diploma.critical_data_logger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriticalDataLoggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
